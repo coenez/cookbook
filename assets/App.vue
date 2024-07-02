916f4d7f3cb096f7a33c3bf06cc6a96ed13e5743
coenez <ccpstals@hotmail.com>
2024-07-02 10:21:32 +0200

    Install webpack and vue. Create vue skeleton.

---
+++ assets/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+const count = ref(0);
+const increase = () => { count.value ++;}
+
+</script>
+
+<template>
+  <div>{{message}}</div>
+  <button @click="increase()">Click {{ count }}</button>
+</template>
+
+<style scoped>
+
+</style>
