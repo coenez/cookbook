f25477329a1464d35da7ffbb255def82e9f3faf8
coenez <ccpstals@hotmail.com>
2024-07-02 16:41:24 +0200

    Fix category serialization by ignoring the recipes property on serialization.

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -25,6 +26,7 @@ class Category
      * @var Collection<int, Recipe>
      */
     #[ORM\OneToMany(targetEntity: Recipe::class, mappedBy: 'category', orphanRemoval: true)]
+    #[Ignore]
     private Collection $recipes;
 
     public function __construct()
