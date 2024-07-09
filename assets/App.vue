700a6425c22f30a8672b3967e150495c16a30736
coenez <ccpstals@hotmail.com>
2024-07-09 11:58:15 +0200

    Provide a global search ref to use in child pages

--- assets/App.vue
+++ assets/App.vue
@@ -2,7 +2,11 @@
 
 import MainLayout from "./Layouts/MainLayout.vue";
 import Test from "./Components/Test.vue";
+import {ref, provide} from "vue";
 
+//global search term that will be used in different contexts
+const globalSearchTerm = ref('')
+provide('globalSearchTerm', globalSearchTerm);
 </script>
 
 <template>
