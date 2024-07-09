72f0b1a3b9c0bda39ca38f51160e5d9fc0050972
coenez <ccpstals@hotmail.com>
2024-07-09 12:16:50 +0200

    Set base for filtering component. Fix naming to avoid confusion.

--- assets/Components/Core/Menu.vue
+++ assets/Components/Core/Menu.vue
@@ -2,11 +2,12 @@
   import {ref, inject, computed} from "vue";
   import router from "../../router";
 
-  const navDrawer = ref(false)
+  const showMainNavDrawer = ref(false)
   const globalSearchTerm = inject('globalSearchTerm')
-  const showSearch = ref(false)
+  const showSearchBar = ref(false)
 
-  const showFilter = computed(() => {
+  const showFilterDrawer = ref(false)
+  const showFilterIcon = computed(() => {
     return router.currentRoute.value.name === 'recipeList'
   })
 
@@ -14,23 +15,23 @@
 
 <template>
   <v-app-bar flat color="primary" name="app-bar">
-    <v-app-bar-nav-icon  @click.stop="navDrawer = !navDrawer"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click.stop="showMainNavDrawer = !showMainNavDrawer"></v-app-bar-nav-icon>
     <v-app-bar-title>CookBook</v-app-bar-title>
     <v-spacer></v-spacer>
     <v-text-field
-        v-if="showSearch"
+        v-if="showSearchBar"
         v-model="globalSearchTerm"
         variant="outlined"
         class="mt-5"
         placeholder="Zoeken"
     />
 
-    <v-btn @click.stop="showSearch = !showSearch" icon="mdi-magnify"/>
-    <v-btn v-if="showFilter"  icon="mdi-filter"/>
+    <v-btn @click.stop="showSearchBar = !showSearchBar" icon="mdi-magnify"/>
+    <v-btn v-if="showFilterIcon" @click.stop="showFilterDrawer = !showFilterDrawer" icon="mdi-filter"/>
     <v-btn icon="mdi-account"/>
 
   </v-app-bar>
-  <v-navigation-drawer v-model="navDrawer" location="left" temporary>
+  <v-navigation-drawer v-model="showMainNavDrawer" location="left" temporary>
     <v-list nav>
       <v-list-item v-for="route in router.getRoutes()"
          :to="{name:route.name}"
@@ -38,6 +39,16 @@
          :title="route.meta.label"
          :value="route.name">
       </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+
+  <!--extract to component later -->
+  <v-navigation-drawer v-model="showFilterDrawer" location="right" temporary>
+    <v-list>
+      <v-list-item title="Filters" class="text-primary"></v-list-item>
+    </v-list>
+    <v-list nav>
+      <v-text-field variant="outlined" label="Testing filter tray"/>
     </v-list>
   </v-navigation-drawer>
 </template>
