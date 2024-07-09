5bf5e92a41f778763b8d2b4d917c90a53a0db4f3
coenez <ccpstals@hotmail.com>
2024-07-09 12:01:20 +0200

    Build a composable wait for change service on the searchTerm so this watcher is generic and it can be used by child components. Install lodash for timeout functionality

---
+++ assets/Composables/useGlobalSearchTerm.js
@@ -0,0 +1,15 @@
+import {watch, ref} from "vue";
+import {debounce} from "lodash/function";
+
+const localSearchTerm = ref('')
+
+//watch for the global search term change and assign the value to the localSearchTerm
+export function useGlobalSearchTerm(globalSearchTerm) {
+    watch(globalSearchTerm, debounce(() => {
+        localSearchTerm.value = globalSearchTerm.value
+    }, 1500));
+
+    return localSearchTerm;
+}
+
+
