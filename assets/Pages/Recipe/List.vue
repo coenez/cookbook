571ec7d00ffd507cafba25e91340c48facc82f80
coenez <ccpstals@hotmail.com>
2024-07-09 14:10:46 +0200

    Implement data fetching for the recipe overview. Add a watcher for the searchterm so we automaticly reload

--- assets/Pages/Recipe/List.vue
+++ assets/Pages/Recipe/List.vue
@@ -1,22 +1,48 @@
 <script setup>
 
+import RecipeSummary from "../../Components/Recipe/RecipeSummary.vue";
+import {inject, onMounted, ref, watch} from "vue";
+import axios from "axios";
+import {useGlobalSearchTerm} from "../../Composables/useGlobalSearchTerm";
+import {debounce} from "lodash/function";
+
+const pageSize = 5;
+const data = ref([]);
+const totalItems = ref(0);
+const loading = ref(true);
+const sortBy = defineModel('sortBy')
+const localSearchTerm = useGlobalSearchTerm(inject('globalSearchTerm'));
+const filters = ref({});
+
+function loadItems() {
+  axios.get(getConfig('urls.recipe.list'), {
+    params: {
+      // orderBy: sortBy.value[0]?.key + '|' + sortBy.value[0]?.order,
+      limit: pageSize,
+      search: localSearchTerm.value,
+      filters: filters
+    }
+  }).then(response => {
+    data.value = response.data;
+    totalItems.value = response.data?.length;
+    loading.value = false;
+  })
+}
+
+onMounted(loadItems)
+
+//watch local searchterm change to trigger a reload
+watch(localSearchTerm, debounce(() => {
+  loadItems()
+}, 1000));
+
 </script>
 
 <template>
   <v-card flat>
-    <v-card-title class="text-primary">Recepten</v-card-title>
+    <v-card-title class="text-secondary">Recepten</v-card-title>
     <v-card-text>
-      overview of recipes here please
-
-
+      <RecipeSummary v-for="recipe in data" :recipe="recipe"></RecipeSummary>
     </v-card-text>
-
-
-
   </v-card>
-
 </template>
-
-<style scoped>
-
-</style>
