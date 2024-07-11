c0300d5dab7bfa08888948cca0d3a52605851654
coenez <ccpstals@hotmail.com>
2024-07-11 15:36:27 +0200

    Fix unit repository to use the global trait

--- src/Repository/UnitRepository.php
+++ src/Repository/UnitRepository.php
@@ -11,33 +11,10 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class UnitRepository extends ServiceEntityRepository
 {
+    use GlobalRepositoryTrait;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Unit::class);
     }
-
-    //    /**
-    //     * @return Unit[] Returns an array of Unit objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Unit
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
