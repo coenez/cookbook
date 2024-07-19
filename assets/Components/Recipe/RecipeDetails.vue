41e6d4829832104cae119d3b40d0184e02b93e48
coenez <ccpstals@hotmail.com>
2024-07-19 13:38:30 +0200

    Split ingredients into 2 columns

--- assets/Components/Recipe/RecipeDetails.vue
+++ assets/Components/Recipe/RecipeDetails.vue
@@ -39,16 +39,23 @@ onMounted(() => {
         <v-progress-circular v-if="!ingredients.length" class="mx-auto" size="100" width="10" color="primary" indeterminate/>
       </div>
 
-      <v-list density="compact" v-if="ingredients.length > 0">
-        <v-list-item v-for="(ingredient) in ingredients">
-          <template v-slot:prepend>
-            <v-list-item-action start>
-              <v-checkbox-btn></v-checkbox-btn>
-            </v-list-item-action>
-          </template>
-            {{ingredient.name}}: {{ingredient.amount}} {{ingredient.unit}}
-        </v-list-item>
-      </v-list>
+      <v-row>
+        <v-col cols="6" v-for="colCount in [1,2]">
+          <v-list density="compact" v-if="ingredients.length > 0">
+            <template v-for="(ingredient, index) in ingredients">
+              <v-list-item v-if="(colCount===1 ? index%2===0 : index%2!==0)">
+                <template v-slot:prepend>
+                  <v-list-item-action start>
+                    <v-checkbox-btn></v-checkbox-btn>
+                  </v-list-item-action>
+                </template>
+                {{ingredient.name}}: {{ingredient.amount}} {{ingredient.unit}}
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-col>
+      </v-row>
+
     </v-card-text>
   </v-card>
   <v-card flat>
