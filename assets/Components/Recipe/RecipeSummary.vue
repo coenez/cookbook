a19234accd1690c4ed19f12671f154cc15fe7263
coenez <ccpstals@hotmail.com>
2024-07-20 15:01:08 +0200

    Make title from summary also clickable

--- assets/Components/Recipe/RecipeSummary.vue
+++ assets/Components/Recipe/RecipeSummary.vue
@@ -1,16 +1,22 @@
 <script setup>
 import RecipeSubTitle from "./RecipeSubTitle.vue";
+import {useRouter} from "vue-router";
 
 const props = defineProps({
   recipe: Object,
 })
 
+const router = useRouter()
 const textSummary = props.recipe.preparation.slice(0, 200) + '...'
+
+const gotoRecipe = (id) => {
+  router.push({name:'recipeView', params: {id:id}});
+}
 </script>
 
 <template>
   <v-card class="mb-8 cardOutline" border="thin" color="text" variant="outlined">
-    <v-card-title class="text-primary ">{{recipe.name}} </v-card-title>
+    <v-card-title class="text-primary cursor-pointer" @click="gotoRecipe(recipe.id)"> {{recipe.name}} </v-card-title>
     <RecipeSubTitle :recipe="recipe"/>
     <v-card-text class="mt-n4">
       {{textSummary}}
