f706a2989d2f3fbc66202bd525f4221bdec0c5ca
coenez <ccpstals@hotmail.com>
2024-07-21 13:05:24 +0200

    migration for not nullable columns

---
+++ migrations/Version20240721094439.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240721094439 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipe_ingredient CHANGE recipe_id recipe_id INT NOT NULL, CHANGE ingredient_id ingredient_id INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipe_ingredient CHANGE recipe_id recipe_id INT DEFAULT NULL, CHANGE ingredient_id ingredient_id INT DEFAULT NULL');
+    }
+}
